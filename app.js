a84a87462fdd25fe6fb84af1e73ac553130341b3
Nico Suarez <nsuarez@umich.edu>
2016-06-30 13:54:13 -0700

    FINALLY SET UP

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '20535ac1ce784763a79e16c952b9cfe8'; // Your client id
+var client_secret = 'f19da400224c4f968acaf580111f534e'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
@@ -35,7 +35,7 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/templates'))
    .use(cookieParser());
 
 app.get('/login', function(req, res) {
